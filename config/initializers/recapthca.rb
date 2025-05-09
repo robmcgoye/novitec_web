c647fb181b6352116d1f4fb29c96998fc9a2a844
rob <rob@mcgoye.com>
2025-05-09 17:41:12 +0000

    updating vars in app

--- config/initializers/recapthca.rb
+++ config/initializers/recapthca.rb
@@ -1,6 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = "6Lc_fzArAAAAAKsShwqiG7i__gTkMkNq1SEhHYOu"
-  config.secret_key = "6Lc_fzArAAAAALf4T84XZEN2Knkx0gabzoIMn1Lf"
-  # config.site_key  = "6Lf-LzArAAAAAAgRbLm1gyUUP3p7Vgy2l_R597if"
-  # config.secret_key = "6Lf-LzArAAAAAPnEwUM76uzdJsWz8VjCRxRjOmeG"
+  config.site_key  = Rails.application.credentials.dig(:recaptcha, :site_key)
+  config.secret_key = Rails.application.credentials.dig(:recaptcha, :secret_key)
 end
